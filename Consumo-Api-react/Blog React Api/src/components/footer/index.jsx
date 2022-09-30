f52f5ce2a1297057e5ecb5cf48f51669de7650a5
Wendel Gustavo <wendelfi66@gmail.com>
2022-09-30 00:23:44 -0300

    Add files via upload

---
+++ Consumo-Api-react/Blog React Api/src/components/footer/index.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+
+const Foot = () => (
+<footer class="text-center text-lg-start bg-light text-muted">
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <div class="me-5 d-none d-lg-block">
+      <span>Blog da API do sor</span>
+    </div>
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+  </section>
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+
+      <div class="row mt-3">
+
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Light Dev
+          </h6>
+          <p>
+            Aqui você encontra os melhores conteúdos sobre desenvolvimento web.
+          </p>
+        </div>
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#" class="text-reset">Angular</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">React</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vue "Candido ama"</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Laravel</a>
+          </p>
+        </div>
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <h6 class="text-uppercase fw-bold mb-4">
+            Links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Suporte</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Configuraões</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Outros</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ajuda!</a>
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">Contatos</h6>
+          <p><i class="fas fa-home me-3"></i> ParobretCity 25/02/2022</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            wendelfi66@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 99 9999-9999</p>
+          <p><i class="fas fa-print me-3"></i> + 99 9999-9999</p>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Light Dev</a>
+  </div>
+
+</footer>
+
+);
+export default Foot;
